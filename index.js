e2196f2ed70ab8384b427cd8da35f233e00fe352
Cristiano Faustino <c_cfaustino@ojolabs.com>
2022-01-15 15:03:10 -0800

    adjusting express static file serve route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express")
+const path = require('path')
 const app = express()
 const port = process.env.PORT || 4000
 
-app.use(express.static('src'))
+app.use(express.static(path.join(__dirname, './src')))
 app.listen(port, () => console.log(`The server is listening on port ${port}`))
