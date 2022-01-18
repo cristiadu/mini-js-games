5226b25e4ec5a17aec3bcccac0ce52e25fdfc31e
Cristiano Faustino <c_cfaustino@ojolabs.com>
2022-01-17 21:33:10 -0800

    adjusting routes

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const path = require('path')
 const app = express()
 const port = process.env.PORT || 4000
 
-app.use(express.static(path.join(__dirname, './src')))
+process.env.PWD = process.cwd();
+app.use(express.static(path.join(process.env.PWD, 'src')));
 app.listen(port, () => console.log(`The server is listening on port ${port}`))
