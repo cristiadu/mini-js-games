6824b1758b08c855a457ed519abd5279c4a10648
Cristiano Faustino <cristiadu@gmail.com>
2026-07-02 14:49:08 -0700

    fix(pong): bounce angle computed from the wrong paddle (#135)

--- src/games/pong/js/globalVariables.js
+++ src/games/pong/js/globalVariables.js
@@ -23,9 +23,6 @@ export const THICKNESS_PADDLE = 10
 export const SPEED_PADDLE = 4
 export const REDUCED_SPEED_AI = 2
 
-export const PADDLE_INITIAL_X = 0
-export const PADDLE_INITIAL_Y = 0
-
 export const BALL_COLOR = '#A288E3'
 export const PADDLE_COLOR = '#E5DCC5'
 export const SCREEN_BACKGROUND_COLOR = '#000'
